53b5221fe7498a6a548f115760507d9468aa0329
vinit-churi <vinitchuri@hotmail.com>
2022-04-08 18:58:51 +0530

    break

--- main.js
+++ main.js
@@ -3,3 +3,31 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // const taskElement = document.importNode(taskTemplate.content, true);
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+const renderView = (jobs) => {
+  jobs.forEach((job) => {
+    console.log(job.location.display_name);
+    console.log(job.company.display_name);
+    console.log(job.category.label);
+    console.log(job.contract_time);
+    console.log(job.created);
+    console.log(job.description);
+  });
+};
+
+const getJobs = async () => {
+  const data = await fetch("http://localhost:3000/jobs", requestOptions)
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
+
+  console.log(data.results);
+
+  renderView(data.results);
+};
+
+getJobs();
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const axios = require("axios").default;
 const { json } = require("express");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 // middleware
 
@@ -14,34 +16,11 @@ app.get("/jobs", (req, res) => {
   res.json(jobs);
 });
 
-// app.use('/api/v1/jobs', jobsRouter);
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is running on ${port}...`);
 });
-
-// "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=13fa0c16&app_key=288b9049cf804e91045b9dfa5b280b21";
-
-updateJobs();
-var intervalId = global.setInterval(function () {
-  updateJobs();
-}, 1000 * 60 * 60 * 24);
-
-var configGeo = {
-  method: "get",
-  url: `http://api.positionstack.com/v1/reverse?access_key=${process.env.ACCESS_KEY}&query=40.7638435,-73.9729691&limit=1`,
-  headers: {},
-};
-
-// axios(configGeo)
-//   .then(function (response) {
-//     console.log(JSON.stringify(response.data));
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
 
 async function updateJobs() {
   const config = {
@@ -61,7 +40,9 @@ async function updateJobs() {
       console.log(error);
     });
 }
-/* not need the location was present */
-// async function updateJobsLocation() {
-//   console.log(jobs);
-// }
+
+let intervalId = global.setInterval(function () {
+  updateJobs();
+}, 1000 * 60 * 60 * 24);
+
+updateJobs();
